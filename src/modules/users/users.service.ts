361b8bd1f0d6911bcf94f2403d339498f663d743
phuongNgo <nxphuongktvt@gmail.com>
2024-02-15 02:33:19 +0700

    first commit

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -34,7 +34,7 @@ export class UsersService {
 
   async findAll(param: GetUsersDto): Promise<ResPagingDto<User[]>> {
     const { sort, page, limit } = param;
-    console.log("in")
+    console.log("in...")
 
     const query: any = {};
     query.deleted = false;
